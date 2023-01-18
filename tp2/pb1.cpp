6c71ce96c86d3c3ee8dbc583f42841c4b9ff307a
Thibaut Delahaie <thibaut.delahaie@polymtl.ca>
2023-01-18 12:53:38 -0500

    Modification du code

--- tp2/pb1.cpp
+++ tp2/pb1.cpp
@@ -20,92 +20,71 @@
 +------+---+--------------+---+
 */
 
-
 #define F_CPU 8000000UL
-#include <avr/io.h>
+#include <avr/io.h> 
 #include <util/delay.h>
 
-#define DDR_IN 0x00
-#define DDR_OUT 0xff
-#define DEBOUNCE_TIME 20
-enum State {E0, E1, E2, E3};
-enum Button {INIT, PRESSING, RELEASE};
+using namespace std;
 
-bool isButtonPressed(){
+#define DDR_IN 0x00;
+#define DDR_OUT 0xff;
+
+enum class State {
+  E0, E1, E2, E3
+};
+
+bool isButtonPushed(){
   if (PIND & (1<<PD2)){
-          _delay_ms(DEBOUNCE_TIME);
+          _delay_ms(20);
           if(PIND & (1<<PD2)) {
-              return true;
+            while(PIND & (01<<PD2))
+              continue;
+            return true;
           }
       }
       return false;
-}
-
-bool isButtonReleased(){
-    // if (PIND & 0X04) {
-    auto button = Button::INIT;
-    switch(button){
-      case INIT:
-        if (isButtonPressed()){
-          button = Button::PRESSING;
-        }
-
-        return false;
-        break;
-
-      case PRESSING:
-        if(!isButtonPressed()){
-        button = Button::RELEASE;
-        }
-
-        return false;
-        break;
-
-      case RELEASE:
-        return true;
-        button = Button::INIT;
-
-        break;
-      }
 }
 
 int main() {
   uint8_t ETEINT = 0x00;
   uint8_t VERT = 0X01;
   //uint8_t ROUGE = 0X02;
-  DDRA = DDR_OUT;
-  DDRD = DDR_IN;
+  DDRA = DDR_OUT;// PORT A est en mode sortie
+  DDRD = DDR_IN; // PORT D est en mode entrée
+  State state {State::E0};
 
-  auto state = State::E0;
+  
   while(true){
     switch(state){
-      case E0:
+      case(State::E0):
         PORTA = ETEINT;
-        if(isButtonReleased()){
+        if(isButtonPushed()){
         state = State::E1;
         }
         break;
 
-      case E1:
+      case(State::E1):
         PORTA = ETEINT;
-        if(isButtonReleased()){
+        if(isButtonPushed()){
           state = State::E2;
         }
         break;
 
-      case E2:
+      case(State::E2):
         PORTA = ETEINT;
-        if(isButtonReleased()){
+        if(isButtonPushed()){
           state = State::E3;
         }
         break;
 
-      case E3:
+      case(State::E3):
         PORTA = VERT;
         _delay_ms(2000);
-        PORTA = ETEINT;
         state = State::E0;
+        break;
 
+        default:
+        state = State::E0;
         break;
       }
   }
